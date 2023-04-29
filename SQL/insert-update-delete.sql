ac029832c4549bf5f64007f19460a9e8a7c6df9a
omeryilldirim <omeryilldirim@outlook.com>
2023-04-30 01:28:55 +0300

    SQL

---
+++ SQL/insert-update-delete.sql
@@ -0,0 +1,21 @@
+/*
+-- INSERT -- Kayıt ekleme
+INSERT INTO student (first_name, last_name, age)
+VALUES ('John', 'Doe', 30)
+*/
+/*
+-- UPDATE -- Kayıt Güncelleme
+UPDATE student SET first_name = 'david', last_name ='scofield' WHERE id = 2;
+*/
+/*
+-- INSERT -- Çoklu Kayıt Ekleme
+INSERT INTO student (first_name, last_name, age)
+VALUES 
+	('Angela', 'Yu', 31),
+	('Angela', 'Yu', 32),
+	('Angela', 'Yu', 33);
+*/
+/*
+-- DELETE -- Kayıt Silme
+DELETE FROM student WHERE id = 5;
+*/
