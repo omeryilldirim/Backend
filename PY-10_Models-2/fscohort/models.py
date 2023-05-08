0865d46a725f88b4d63d5a8ac6bd6a1f04b65471
omeryilldirim <omeryilldirim@outlook.com>
2023-05-08 22:26:42 +0300

    models-shell

--- PY-10_Models-2/fscohort/models.py
+++ PY-10_Models-2/fscohort/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+# https://docs.djangoproject.com/en/4.2/topics/db/models/
+class Student(models.Model):
+    # https://docs.djangoproject.com/en/4.2/ref/models/fields/#field-types
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(null=True, blank=True)
+    number = models.IntegerField(null=True, blank=True)
+    picture = models.ImageField(upload_to='images/', default='', null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True) # Kayıt yapıldığı anki zamanı otomatik yaz.
+    updated = models.DateTimeField(auto_now=True) # Kayıt güncellendiği anki zamanı otomatik yaz.
+
+    def __str__(self): # Kayıt yazdır.
+        return f'{self.first_name} {self.last_name} # {self.number}'
+    
+    class Meta: # Default özellikleri değiştir.
+        # https://docs.djangoproject.com/en/4.2/ref/models/options/
+        verbose_name = 'Öğrenci'
+        verbose_name_plural = 'Öğrenciler'
+        ordering = ["-first_name"] # Ters sıralam için (DESC) sutun isminin başına - konur.
+
+# ---------------------------- Django Shell ----------------------------
+
+AGES=(
+    (10, 'age:10'),
+    (20, 'age:20'),
+    (30, 'age:30'),
+    (40, 'age:40'),
+    (50, 'age:50'),
+)
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    age = models.PositiveIntegerField(default=0, null=True, blank=True, choices=AGES)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
--- PY-10_Models-2/main/settings.py
+++ PY-10_Models-2/main/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'fscohort',
 ]
 
 MIDDLEWARE = [
@@ -116,6 +117,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / STATIC_URL
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / MEDIA_URL
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
