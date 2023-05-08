808b008f8d50b0c5c6b0457916fcb16d88ab903d
omeryilldirim <omeryilldirim@outlook.com>
2023-05-08 20:33:41 +0300

    ws

--- Teamwork-2/ws_project/test_app/views.py
+++ Teamwork-2/ws_project/test_app/views.py
@@ -3,6 +3,6 @@ from django.http import HttpResponse
 
 # Create a view called home
 def home(request):
-    return HttpResponse("<h1 color='red'>Hello, Django!</h1>")
+    return HttpResponse("<h1 style='color:red'>Hello, Django!</h1>")
 
 
