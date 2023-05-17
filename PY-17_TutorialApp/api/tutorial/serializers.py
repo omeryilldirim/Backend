ae2333caae9371069fc74d34977c66a1585c306f
omeryilldirim <omeryilldirim@outlook.com>
2023-05-17 21:01:15 +0300

    tutorial

---
+++ PY-17_TutorialApp/api/tutorial/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import Tutorial
+
+class TutorialSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tutorial
+        fields = '__all__'
+
+    def validate(self, data):
+        if data['title'] == data['description']:
+            raise serializers.ValidationError("Title and Description should be different")
+        return data
--- PY-17_TutorialApp/api/tutorial/views.py
+++ PY-17_TutorialApp/api/tutorial/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Tutorial
+from .serializers import TutorialSerializer
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+class TutorialViewSet(ModelViewSet):
+    queryset = Tutorial.objects.all()
+    serializer_class = TutorialSerializer
+
