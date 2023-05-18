a9e5ddc296d1be944560c7051f7d360926b44903
omeryilldirim <omeryilldirim@outlook.com>
2023-05-18 21:03:40 +0300

    delete

--- PY-18_BlogAPI/blog/urls.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from django.urls import path, include
-from .views import PostViewSet, CategoryViewSet
-
-router = DefaultRouter()
-router.register('posts', PostViewSet)
-router.register('categories', CategoryViewSet)
-
-url_patterns = router.urls
