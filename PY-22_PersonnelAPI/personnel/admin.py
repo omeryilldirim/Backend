3ab3e66fb2e0afdd041f4700de6fca1a64c9d46d
omeryilldirim <omeryilldirim@outlook.com>
2023-05-31 02:06:42 +0300

    models

--- PY-22_PersonnelAPI/personnel/admin.py
+++ PY-22_PersonnelAPI/personnel/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Personnel, Department
 
-# Register your models here.
+admin.site.register(Department)
+admin.site.register(Personnel)
--- PY-22_PersonnelAPI/personnel/models.py
+++ PY-22_PersonnelAPI/personnel/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Department(models.Model):
+    name = models.CharField(max_length=32)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.name}"
+    
+
+class Personnel(models.Model):
+    first_name = models.CharField(max_length=32)
+    last_name = models.CharField(max_length=32)
+    GENDER = (
+        ('F', 'Female'),
+        ('M', 'Male'),
+        ('N', 'Prefer not to say'),
+    )
+    gender = models.CharField(max_length=1, choices=GENDER)
+    TITLE = (
+        ('S', 'Senior'),
+        ('M', 'Mid-senior'),
+        ('J', 'Junior'),
+    )
+    title = models.CharField(max_length=1, choices=TITLE)
+    salary = models.IntegerField()
+    started = models.DateField()
+    department = models.ForeignKey(Department, on_delete=models.SET_NULL, null=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+    
