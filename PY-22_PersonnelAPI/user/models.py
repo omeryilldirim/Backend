2ea3afb9390a874dd2f7d7038c7e04c23cc46b1a
omeryilldirim <omeryilldirim@outlook.com>
2023-06-01 17:32:21 +0300

    personnel api

--- PY-22_PersonnelAPI/user/models.py
+++ PY-22_PersonnelAPI/user/models.py
@@ -2,9 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Profile(models.Model):
-    image = models.ImageField(upload_to='images/', default='avatar.png')
+    image = models.ImageField(upload_to='images', default='avatar.png')
     about = models.TextField(blank=True, null=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.user.first_name
+        return self.user.username
