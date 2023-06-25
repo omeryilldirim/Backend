55395af221923d5acce836611a2d6ca5be5d0f5a
omeryilldirim <omeryilldirim@outlook.com>
2023-06-25 14:56:04 +0300

    sql base dir updated

--- PY-28_StockAppAPI/main/settings/base.py
+++ PY-28_StockAppAPI/main/settings/base.py
@@ -14,7 +14,7 @@ from pathlib import Path
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
